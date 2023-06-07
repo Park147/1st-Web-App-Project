cf70f0fb28763ed33d33e967e25bffb2f2688362
Jobs11 <mnx752@gmail.com>
2023-06-07 16:43:25 +0900

    뒤로가기와 설정창 가기 추가

---
+++ app/src/main/java/com/example/teamproject/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.teamproject.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.teamproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
