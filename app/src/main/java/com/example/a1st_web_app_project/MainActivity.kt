cf67ef4ed231d65883bcfa4295e432582dab96b1
ghkdwlals0 <ghkdwlals0@naver.com>
2023-06-07 16:43:20 +0900

    3 commit

--- app/src/main/java/com/example/a1st_web_app_project/MainActivity.kt
+++ app/src/main/java/com/example/a1st_web_app_project/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.a1st_web_app_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ActionMenuView
+import android.widget.TextView
+import com.example.a1st_web_app_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    var Toorbarname: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.toolbar.title = "main"
+
     }
-}첫 커밋
-        두번째 커밋 시도중. 6/7
-        왜 에러가 뜨는건지?
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionmenu, menu)
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+
+        return true
+    }
+
+}
