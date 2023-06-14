17488cd1f815b0bc2fc20e7ee2cfcbe12b65d452
jiminhwang94 <126554189+jiminhwang94@users.noreply.github.com>
2023-06-14 22:28:12 +0900

    0614 집에서 푸쉬함. 메인 디자인 수정 !!!

--- app/src/main/java/com/example/a1st_web_app_project/adapter/MyAdapter.kt
+++ app/src/main/java/com/example/a1st_web_app_project/adapter/MyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+
 import com.example.a1st_web_app_project.databinding.ItemMainBinding
 import com.example.a1st_web_app_project.model.RstrModel
 
